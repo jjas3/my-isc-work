a85e930cb90dc4ef5b83b53903ba704d163f56b8
User 01 <user01@unst08.(none)>
2017-11-14 16:28:30 +0000

    some loop files

---
+++ if.py
@@ -0,0 +1,8 @@
+name = raw_input("Input name: ")
+if name == "Lisa":
+    print name + " plays"
+elif name == "Bart":
+    print name + " rides"
+else:
+    print "nobody knows who you are, sorry"
+
---
+++ while_loop.py
@@ -0,0 +1,6 @@
+x=1
+while x<100:
+    x +=1
+    print x
+    print 'next'
+
