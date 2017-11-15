931c2c70befd9a1bda81bc142374500cff896ff2
User 01 <user01@unst08.(none)>
2017-11-15 13:49:49 +0000

    Strings stuff

---
+++ loop_string_as_sequence.py
@@ -0,0 +1,24 @@
+#s ="I Love working in Python"
+#split_s=s.split()
+#
+#print split_s
+#
+#for word in split_s:
+#    if word.find("i") > -1:
+#        print "I found 'i' in: '{0}'".format(word)
+
+#exploring useful aspects of strings
+
+something = "completely different"
+
+print dir(something)
+
+print something.count("t")
+print something.find("plet")
+print something.split("e")
+thing2= something.replace("different","silly")
+print thing2
+
+
+
+   
